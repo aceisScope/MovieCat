0e7b85de3379c1a02f294aa37eee199be545c3e3
Binghui Liu <bertieliu52@gmail.com>
2017-07-28 12:39:09 +0300

    construct discover url with sort_by key

--- app/src/main/java/com/example/binghuiliu/moviecat/MainActivity.java
+++ app/src/main/java/com/example/binghuiliu/moviecat/MainActivity.java
@@ -6,7 +6,11 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
+import com.example.binghuiliu.moviecat.utils.NetworkUtils;
+
 public class MainActivity extends AppCompatActivity implements RecyclerViewAdapter.OnItemClickListener{
+
+    private final String DEBUG = "DEBUG";
 
     private RecyclerView recyclerView;
     private RecyclerViewAdapter adapter;
@@ -20,12 +24,15 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewAdapt
         int numberOfColumns = 2;
         recyclerView.setLayoutManager(new GridLayoutManager(this, numberOfColumns));
         adapter = new RecyclerViewAdapter(this, null, this);
-        //adapter.setClickListener(this);
         recyclerView.setAdapter(adapter);
+
+        NetworkUtils networkUtils = new NetworkUtils(this);
+        String url = networkUtils.discoverUrlSortBy(getString(R.string.sort_popular));
+        Log.d(DEBUG, url);
     }
 
     @Override
     public void onItemClick(int position) {
-        Log.d("DEBUG", "Click on " + Integer.toString(position));
+        Log.d(DEBUG, "Click on " + Integer.toString(position));
     }
 }
--- app/src/main/java/com/example/binghuiliu/moviecat/utils/NetworkUtils.java
+++ app/src/main/java/com/example/binghuiliu/moviecat/utils/NetworkUtils.java
@@ -1,16 +1,52 @@
 package com.example.binghuiliu.moviecat.utils;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.net.Uri;
+
+import com.example.binghuiliu.moviecat.MainActivity;
+import com.example.binghuiliu.moviecat.R;
+
+import java.net.URL;
+
 /**
  * Created by binghuiliu on 28/07/2017.
  */
 
 public class NetworkUtils {
 
-    private static final String authority = "api.themoviedb.org";
-    private static final String v3 = "3";
-    private static final String movile = "movie";
+    public static final String popular = "popular";
 
-    private static final String popular = "popular";
-    private static final String api_key = "api_key";
+    private final String authority = "api.themoviedb.org";
+    private final String v3 = "3";
+    private final String discover = "discover";
+    private final String movile = "movie";
+
+    private final Context mContext;
+
+    private final String api_key = "api_key";
+    private final String sort_by = "sort_by";
+    private final String api_key_value;
+
+    public NetworkUtils(Context context) {
+        this.mContext = context;
+        this.api_key_value = context.getString(R.string.key_value);
+    }
+
+    public String discoverUrlSortBy(String sort) {
+        Uri.Builder builder = new Uri.Builder();
+
+        builder.scheme("https")
+                .authority(authority)
+                .appendPath(v3)
+                .appendPath(discover)
+                .appendPath(movile)
+                .appendQueryParameter(sort_by, sort)
+                .appendQueryParameter(api_key, api_key_value);
+
+        String myURL = builder.build().toString();
+
+        return myURL;
+    }
 
 }

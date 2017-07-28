34efe0d62ca8161e6327532dea6f154087ae1870
Binghui Liu <bertieliu52@gmail.com>
2017-07-28 11:21:25 +0300

    create networkutil class

---
+++ app/src/main/java/com/example/binghuiliu/moviecat/utils/NetworkUtils.java
@@ -0,0 +1,16 @@
+package com.example.binghuiliu.moviecat.utils;
+
+/**
+ * Created by binghuiliu on 28/07/2017.
+ */
+
+public class NetworkUtils {
+
+    private static final String authority = "api.themoviedb.org";
+    private static final String v3 = "3";
+    private static final String movile = "movie";
+
+    private static final String popular = "popular";
+    private static final String api_key = "api_key";
+
+}
